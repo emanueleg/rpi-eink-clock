475d8040843b4c038424bf8f096b329151774df5
Emanuele Goldoni <emanuele.goldoni@gmail.com>
2020-08-12 17:54:58 +0200

    update main script
    New code uses latest grayscale 2.7in library.
    Display refresh when using red channel and 2.7B library is too slow.

--- epaper-clock.py
+++ epaper-clock.py
@@ -3,54 +3,31 @@
 ##
 # epaper-clock.py
 #
-# Copyright (C) Jukka Aittola (jaittola(at)iki.fi)
+# Copyright (C) Emanuele Goldoni 2019
 #
- # Permission is hereby granted, free of charge, to any person obtaining a copy
- # of this software and associated documnetation files (the "Software"), to deal
- # in the Software without restriction, including without limitation the rights
- # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- # copies of the Software, and to permit persons to  whom the Software is
- # furished to do so, subject to the following conditions:
- #
- # The above copyright notice and this permission notice shall be included in
- # all copies or substantial portions of the Software.
- #
- # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- # FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- # LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- # THE SOFTWARE.
- ##
-
+# original author: Jukka Aittola (jaittola(at)iki.fi) 2017
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documnetation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to  whom the Software is
+# furished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 ##
- #  @filename   :   main.cpp
- #  @brief      :   2.9inch e-paper display (B) demo
- #  @author     :   Yehui from Waveshare
- #
- #  Copyright (C) Waveshare     July 31 2017
- #
- # Permission is hereby granted, free of charge, to any person obtaining a copy
- # of this software and associated documnetation files (the "Software"), to deal
- # in the Software without restriction, including without limitation the rights
- # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- # copies of the Software, and to permit persons to  whom the Software is
- # furished to do so, subject to the following conditions:
- #
- # The above copyright notice and this permission notice shall be included in
- # all copies or substantial portions of the Software.
- #
- # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- # FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- # LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- # THE SOFTWARE.
- ##
 
-import epd2in7b
+
+import epd2in7
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
@@ -62,10 +39,7 @@ import time
 import locale
 import subprocess
 
-COLORED = 1
-UNCOLORED = 0
-
-LOCALE="fi_FI"
+LOCALE="it_IT.UTF8"
 DATEFORMAT = "%a %x"
 TIMEFORMAT = "%H:%M"
 FONT = '/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf'
@@ -78,11 +52,10 @@ class Fonts:
 def main():
     locale.setlocale(locale.LC_ALL, LOCALE)
 
-    epd = epd2in7b.EPD()
+    epd = epd2in7.EPD()
     epd.init()
-    epd.set_rotate(epd2in7b.ROTATE_270)
 
-    fonts = Fonts(timefont_size = 75, datefont_size = 30)
+    fonts = Fonts(timefont_size = 75, datefont_size = 26)
 
     read_button4_for_shutdown()
     clock_loop(epd, fonts)
@@ -99,14 +72,12 @@ def draw_clock_data(epd, fonts, datetime_now):
     datestring = datetime_now.strftime(DATEFORMAT).capitalize()
     timestring = datetime_now.strftime(TIMEFORMAT)
 
-    # Create frame buffers
-    frame_black = [0] * (epd.width * epd.height / 8)
-    frame_red = [0] * (epd.width * epd.height / 8)
+    Limage = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
+    draw = ImageDraw.Draw(Limage)
+    draw.text((20, 20), timestring, font = fonts.timefont, fill = 0)
+    draw.text((20, 100), datestring, font = fonts.datefont, fill = 0)
+    epd.display(epd.getbuffer(Limage))
 
-    epd.draw_string_at(frame_black, 20, 20, timestring, fonts.timefont, COLORED)
-    epd.draw_string_at(frame_black, 20, 100, datestring, fonts.datefont, COLORED)
-#    epd.draw_string_at(frame_red, 50, 120, "e-paper.", font, COLORED)
-    epd.display_frame(frame_black, frame_red)
 
 def read_button4_for_shutdown():
     GPIO.setmode(GPIO.BCM)
