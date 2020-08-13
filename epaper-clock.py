2b4fc83c6728d4be891c9a3a9c2ccd7942f457a2
Emanuele Goldoni <emanuele.goldoni@gmail.com>
2020-08-13 12:02:14 +0200

    code refactoring
    Use Objects instead of functions. Use of a state to track current display mode. Now we can switch between logo, clock and sysstats using buttons.

--- epaper-clock.py
+++ epaper-clock.py
@@ -47,6 +47,7 @@ LOCALE="it_IT.UTF8"
 DATEFORMAT = "%a %x"
 TIMEFORMAT = "%H:%M"
 FONT = '/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf'
+BOUNCETIME = 200
 
 class Fonts:
     def __init__(self, timefont_size, datefont_size, infofont_size):
@@ -54,91 +55,115 @@ class Fonts:
         self.datefont = ImageFont.truetype(FONT, datefont_size)
         self.infofont = ImageFont.truetype(FONT, infofont_size)
 
-def main():
-    locale.setlocale(locale.LC_ALL, LOCALE)
-
-    epd = epd2in7.EPD()
-    epd.init()
-
-    fonts = Fonts(timefont_size = 75, datefont_size = 26, infofont_size = 18)
-
-    read_button2()
-    read_button3()
-    read_button4()
-    draw_rpi_logo(epd)
-    #system_loop(epd, fonts)
-    #clock_loop(epd, fonts)
-
-def go_to_sleep(sleep_secs):
-    now = datetime.now()
-    seconds_until_next_minute = sleep_secs - now.time().second
-    time.sleep(seconds_until_next_minute)
-
-def clock_loop(epd, fonts):
-    while True:
-        now = datetime.now()
-        draw_clock_data(epd, fonts, now)
-        go_to_sleep(60)
-
-def system_loop(epd, fonts):
-    while True:
-        draw_system_data(epd, fonts)
-        go_to_sleep(60)
-
-def draw_rpi_logo(epd):
-    Himage = Image.open(os.path.join('.', 'raspberry.bmp'))
-    epd.display(epd.getbuffer(Himage))
-    while True:
-        go_to_sleep(60)
-
-def draw_clock_data(epd, fonts, datetime_now):
-    datestring = datetime_now.strftime(DATEFORMAT).capitalize()
-    timestring = datetime_now.strftime(TIMEFORMAT)
-
-    Limage = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-    draw = ImageDraw.Draw(Limage)
-    draw.text((20, 20), timestring, font = fonts.timefont, fill = 0)
-    draw.text((20, 100), datestring, font = fonts.datefont, fill = 0)
-    epd.display(epd.getbuffer(Limage))
-
-def draw_system_data(epd, fonts):
-    corestring = str(psutil.cpu_count()) + ' CPU @ ' + str(psutil.cpu_freq().current) + ' MHz';
-    usagestring = 'CPU usage: ' + str(psutil.cpu_percent());
-    memstring = 'RAM: ' + str(int(psutil.virtual_memory().available/(1024*1024))) + ' MiB';
-    tempstring = 'CPU Temp. ' + str(round(psutil.sensors_temperatures(fahrenheit=False)['cpu_thermal'][0].current)) + ' \u00b0C';
-    psstring = 'Running ps: ' + str(len(psutil.pids()))
-    sysstring = corestring + '\n' + usagestring + '\n' + memstring + '\n' + tempstring + '\n' + psstring
+class Display:
     
-    #iflist = [name for name in psutil.net_if_addrs().keys()]
-    netstring = '\n'.join([str(ifname +' '+str(ip.address)) for ifname in psutil.net_if_addrs().keys() for ip in psutil.net_if_addrs()[ifname] if ip.family == socket.AF_INET])
+    epd = None
+    fonts = None
+    mode = 1
+    
+    def __init__(self):
+        locale.setlocale(locale.LC_ALL, LOCALE)
 
-    Limage = Image.new('1', (epd.height, epd.width), 255)
-    draw = ImageDraw.Draw(Limage)
-    draw.text((10, 10), sysstring, font = fonts.infofont, fill = 0)
-    draw.text((10, 110), netstring, font = fonts.infofont, fill = 0)
-    epd.display(epd.getbuffer(Limage))
+        self.epd = epd2in7.EPD()
+        self.epd.init()
+        
+        self.fonts = Fonts(timefont_size = 75, datefont_size = 26, infofont_size = 18)
 
-def read_button2():
-    GPIO.setmode(GPIO.BCM)
-    pin = 6  # 2nd button
-    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(pin, GPIO.FALLING, callback=button_pressed, bouncetime=200)
+        self.read_button1()
+        self.read_button2()
+        self.read_button3()
+        self.read_button4()
+        self.draw_rpi_logo()
 
-def read_button3():
-    GPIO.setmode(GPIO.BCM)
-    pin = 13  # 3rd button
-    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(pin, GPIO.FALLING, callback=button_pressed, bouncetime=200)
+    def start(self):
+        while True:
+            if 2 == self.mode:
+                self.draw_system_data()
+            elif 3 == self.mode:
+                self.draw_clock_data()
+            else:
+                self.draw_rpi_logo()
+            self.sleep1min()
+    
+    def sleep1min(self):
+        now = datetime.now()
+        seconds_until_next_minute = 60 - now.time().second
+        time.sleep(seconds_until_next_minute)
 
-def read_button4():
-    GPIO.setmode(GPIO.BCM)
-    pin = 19  # 4th button in the 2.7 inch hat this pin according to the schematics.
-    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(pin, GPIO.FALLING, callback=button_pressed, bouncetime=200)
+    def draw_rpi_logo(self):
+        Himage = Image.open(os.path.join('.', 'raspberry.bmp'))
+        self.epd.display(self.epd.getbuffer(Himage))
 
-def button_pressed(pin):
-    print("Button %d was pressed..." % pin)
-    #subprocess.call(["sudo", "poweroff"])
+
+    def draw_clock_data(self):
+        datetime_now = datetime.now()
+        datestring = datetime_now.strftime(DATEFORMAT).capitalize()
+        timestring = datetime_now.strftime(TIMEFORMAT)
+
+        Limage = Image.new('1', (self.epd.height, self.epd.width), 255)  # 255: clear the frame
+        draw = ImageDraw.Draw(Limage)
+        draw.text((20, 20), timestring, font = self.fonts.timefont, fill = 0)
+        draw.text((20, 100), datestring, font = self.fonts.datefont, fill = 0)
+        self.epd.display(self.epd.getbuffer(Limage))
+
+    def draw_system_data(self):
+        corestring = str(psutil.cpu_count()) + ' CPU @ ' + str(psutil.cpu_freq().current) + ' MHz';
+        usagestring = 'CPU usage: ' + str(psutil.cpu_percent());
+        memstring = 'RAM: ' + str(int(psutil.virtual_memory().available/(1024*1024))) + ' MiB';
+        tempstring = 'CPU Temp. ' + str(round(psutil.sensors_temperatures(fahrenheit=False)['cpu_thermal'][0].current)) + ' \u00b0C';
+        psstring = 'Running ps: ' + str(len(psutil.pids()))
+        sysstring = corestring + '\n' + usagestring + '\n' + memstring + '\n' + tempstring + '\n' + psstring
+        
+        #iflist = [name for name in psutil.net_if_addrs().keys()]
+        netstring = '\n'.join([str(ifname +' '+str(ip.address)) for ifname in psutil.net_if_addrs().keys() for ip in psutil.net_if_addrs()[ifname] if ip.family == socket.AF_INET])
+
+        Limage = Image.new('1', (self.epd.height, self.epd.width), 255)
+        draw = ImageDraw.Draw(Limage)
+        draw.text((10, 10), sysstring, font = self.fonts.infofont, fill = 0)
+        draw.text((10, 110), netstring, font = self.fonts.infofont, fill = 0)
+        self.epd.display(self.epd.getbuffer(Limage))
+
+
+    def read_button1(self):
+        GPIO.setmode(GPIO.BCM)
+        pin = 5  # 2nd button
+        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(pin, GPIO.FALLING, callback=self.button_pressed, bouncetime=BOUNCETIME)
+
+    def read_button2(self):
+        GPIO.setmode(GPIO.BCM)
+        pin = 6  # 2nd button
+        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(pin, GPIO.FALLING, callback=self.button_pressed, bouncetime=BOUNCETIME)
+
+    def read_button3(self):
+        GPIO.setmode(GPIO.BCM)
+        pin = 13  # 3rd button
+        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(pin, GPIO.FALLING, callback=self.button_pressed, bouncetime=BOUNCETIME)
+
+    def read_button4(self):
+        GPIO.setmode(GPIO.BCM)
+        pin = 19  # 4th button in the 2.7 inch hat this pin according to the schematics.
+        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(pin, GPIO.FALLING, callback=self.button_pressed, bouncetime=BOUNCETIME)
+
+    def button_pressed(self, pin):
+        #print("Button %d was pressed..." % pin)
+        if 5 == pin:
+            self.draw_rpi_logo()
+            self.mode = 1
+        elif 6 == pin:
+            self.draw_system_data()
+            self.mode = 2
+        elif 13 == pin:
+            self.draw_clock_data()
+            self.mode = 3
+        elif 19 == pin:
+            self.mode = 4
+            print("Shutting down system...")
+            #subprocess.call(["sudo", "poweroff"])
 
 if __name__ == '__main__':
-    main()
+    display = Display()
+    display.start()
