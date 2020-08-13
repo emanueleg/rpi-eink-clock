1a285353e120afca18e326debc70ca17c941fff9
Emanuele Goldoni <emanuele.goldoni@gmail.com>
2020-08-13 17:27:25 +0200

    new code, new license
    Code is not anymore a modified version - quite everything has new been writtern from scratch, so I'm free to change the license for my own code

--- epaper-clock.py
+++ epaper-clock.py
@@ -1,29 +1,11 @@
 #!/usr/bin/env python3
 
 ##
-# epaper-clock.py
+# epaper-clock.py - smart clock / system monitor and more
 #
 # Copyright (C) Emanuele Goldoni 2020
 #
-# original author: Jukka Aittola (jaittola(at)iki.fi) 2017
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documnetation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to  whom the Software is
-# furished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+# Released under the Apache License, Version 2.0
 ##
 
 
